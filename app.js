4d3f66946819b5f08b258b58f36e9815c414745e
kvoronov <bestchemkondrat@gmail.com>
2017-05-15 02:52:20 +0300

    ci test

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
     res.send('Hello Volunteer!');
 });
 
+app.get('/test_ci', (req, res)=>{
+    res.send('test ci')
+})
+
 app.listen(app.get('port'), function () {
     console.log('Example app listening on port', app.get('port'));
 });

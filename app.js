7ab5af9c03dfaaeb96dc30db947ca57f3c835aff
Alexander Gor <ornot.work@gmail.com>
2017-05-15 22:22:28 +0300

    Card model, mongoose, card url

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 "use strict";
 
 var express = require('express');
+var mongoose = require('mongoose');
+
 var app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
 
 app.set('port', (process.env.PORT || 3000));
 
-app.get('/', function (req, res) {
-    res.send('Hello Volunteer!');
-});
-
-app.get('/test_ci', (req, res)=>{
-    res.send('test ci')
-})
+require('./routes.js')(app);
 
 app.listen(app.get('port'), function () {
     console.log('Example app listening on port', app.get('port'));

adb546986d42f0e815e12b73998f0c750ae9e072
Alexander Gor <ornot.work@gmail.com>
2017-05-21 20:07:05 +0300

    User model with roles and coins fields, facebook auth routine

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 "use strict";
 
-var express = require('express');
-var mongoose = require('mongoose');
+let express = require('express');
+let mongoose = require('mongoose');
 
-var app = express();
+let passport = require('passport');
+
+let app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
 
 app.set('port', (process.env.PORT || 3000));
 
-require('./routes.js')(app);
+require('./routes.js')(app, passport);
 
 app.listen(app.get('port'), function () {
     console.log('Example app listening on port', app.get('port'));

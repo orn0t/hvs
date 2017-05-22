7175ef7155b08e78b599cef9b3246b050b26ca0d
Alexander Gor <ornot.work@gmail.com>
2017-05-22 18:00:10 +0300

    Env FB variabes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 let express = require('express');
 let mongoose = require('mongoose');
-
+let session = require('express-session');
 let passport = require('passport');
 
 let app = express();
@@ -11,6 +11,11 @@ mongoose.connect(process.env.MONGODB_URI);
 
 app.set('port', (process.env.PORT || 3000));
 
+app.use(session({ secret: 'wazzzapmakersgonnamake'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./auth.js')(passport);
 require('./routes.js')(app, passport);
 
 app.listen(app.get('port'), function () {

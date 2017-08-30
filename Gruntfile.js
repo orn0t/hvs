448457c97fef69b97f3bfcb497c57bfaf92e2a45
Alexander Gor <ornot.work@gmail.com>
2017-08-30 10:58:24 +0300

    Profile basic layout, showing coins amount

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+/**
+ * Created by orn0t <ornot.work@gmail.com> on 8/30/17.
+ */
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            all: {
+                options: {
+                    paths: ['public/css']
+                },
+            },
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.registerTask('default', ['less']);
+};

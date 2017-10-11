9d8e72842c324ed48d811e94f7c30a9bd440737c
Alexander Gor <ornot.work@gmail.com>
2017-10-11 13:34:57 +0300

    Missions api endpoint added

--- api.js
+++ api.js
@@ -3,6 +3,8 @@
 let express = require('express');
 let router = express.Router();
 
+let Mission = require('./models/mission.js');
+
 module.exports = (passport) => {
     router.all('*', (req, res, next) => {
         passport.authenticate('facebook-token', (err, user, info) => {
@@ -18,6 +20,16 @@ module.exports = (passport) => {
 
     router.get('/v1.0/profile', (req, res) => {
         res.json(req.user);
+    });
+
+    router.get('/v1.0/missions', (req, res) => {
+        Mission.find({}).exec((err, missions) => {
+            if(err) {
+                res.status(500).json({error: err});
+            }
+
+            res.json(missions);
+        });
     });
 
     return router;

fe5a962d4ac2e73b8e2d35ace377af946110cbc3
orn0t <ornot.work@gmail.com>
2017-11-13 12:21:49 +0200

    UVAPP-110. api endpoint for ordering product

--- models/product.js
+++ models/product.js
@@ -2,7 +2,20 @@
 
 let mongoose = require('mongoose');
 
-let productSchema = mongoose.Schema({
+let schemaOrder = mongoose.Schema({
+    created: { type: Date, default: Date.now },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    status: {
+        type: String,
+        enum: ['NEW', 'DECLINED', 'APPROVED'],
+        default: 'NEW'
+    }
+});
+
+let schemaProduct = mongoose.Schema({
     name: String,
     description: String,
     giver: String,
@@ -14,7 +27,8 @@ let productSchema = mongoose.Schema({
         enum: ['REAL', 'VIRTUAL'],
         default: 'REAL'
     },
+    orders: [schemaOrder],
     active: { type: Boolean, default: false }
 });
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model('Product', schemaProduct);
